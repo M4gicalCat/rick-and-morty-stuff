8f59ec651332d5fb0443025d0487379ebfdd8fcb
M4gicalCat <philippefaisandier@gmail.com>
2022-12-23 20:34:33 +0100

    yep

--- src/index.js
+++ src/index.js
@@ -5,27 +5,6 @@ import {Router} from "./Components/Router";
 import {Provider} from "react-redux";
 import store from "./store/store";
 
-import styled from "styled-components";
-
-const Scrollbar = styled.div`
-  display: none;
-  root::-webkit-scrollbar {
-    width: 10px;
-    height: 10px;
-  }
-
-  root::-webkit-scrollbar-track {
-    background: transparent;
-  }
-
-  root::-webkit-scrollbar-thumb {
-    background: #888;
-  }
-
-  root::-webkit-scrollbar-thumb:hover {
-    background: #555;
-  }
-`;
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
